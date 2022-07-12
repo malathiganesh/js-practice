4a8c45865fac1b77ec95bb939890bb572a824a2f
malathiganesh <hamish1617@gmail.com>
2022-07-12 14:48:25 +0530

    task completed

--- mrgeligiblity.js
+++ mrgeligiblity.js
@@ -1,7 +1,16 @@
-age=15;
-if(age>=21)
+
+gen=prompt("enter gender:");
+age=prompt("enter age");
+if(gen=="m")
 {
+    if(age>=21)
+    console.log("eligible for marriage"); 
+    else
+    console.log("not eligible for marriage");
+}
+else if(gen=="f"){
+    if(age>=18)
     console.log("eligible for marriage");
-}else{
+    else
     console.log("not eligible for marriage");
 }
